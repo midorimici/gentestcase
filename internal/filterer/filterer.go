266439bb946bbf3dc5f82030eaa78c3954ed78da
mici <60635066+midorimici@users.noreply.github.com>
2023-07-15 22:40:24 +0900

    ✨ Implement filter

---
+++ internal/filterer/filterer.go
@@ -0,0 +1,49 @@
+package filterer
+
+import (
+	"integtest/internal/condition"
+	"integtest/internal/model"
+)
+
+type Filterer interface {
+	Filter() ([]model.Combination, error)
+}
+
+type filterer struct {
+	cases        model.Cases
+	parser       condition.Parser
+	combinations []model.Combination
+}
+
+func New(cases model.Cases, p condition.Parser, comb []model.Combination) Filterer {
+	return &filterer{cases, p, comb}
+}
+
+func (f *filterer) Filter() ([]model.Combination, error) {
+	const funcName = "filterer.Filter"
+
+	combs := []model.Combination{}
+	for _, comb := range f.combinations {
+		isValidComb := true
+		for element, option := range comb {
+			cond := f.cases[element].Options[option].If
+			if cond == "" {
+				continue
+			}
+
+			ok, err := f.parser.Parse(comb, cond)
+			if err != nil {
+				return nil, err
+			}
+			if ok {
+				continue
+			}
+			isValidComb = false
+			break
+		}
+		if isValidComb {
+			combs = append(combs, comb)
+		}
+	}
+	return combs, nil
+}
--- internal/model/model.go
+++ internal/model/model.go
@@ -3,14 +3,17 @@ package model
 type Cases map[string]Element
 
 type Element struct {
+	Order int
 	Name string
 	Options map[string]Option
 }
 
 type Option struct {
 	Name string
-	Only []string
-	Except []string
+	
+	// If represents condition to output the option.
+	// example: element1.option_id && !element2.option_id
+	If string
 }
 
 type Combination map[string]string
