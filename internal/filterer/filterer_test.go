1c693d5a82b419878174b4f95272368e4848d597
mici <60635066+midorimici@users.noreply.github.com>
2023-07-16 00:21:27 +0900

    ✅ Add filterer test

---
+++ internal/filterer/filterer_test.go
@@ -0,0 +1,79 @@
+package filterer_test
+
+import (
+	"reflect"
+	"testing"
+
+	"integtest/internal/filterer"
+	"integtest/internal/model"
+)
+
+type mockParser struct{}
+
+func (p *mockParser) Parse(combination model.Combination, text string) (bool, error) {
+	e1 := combination["e1"]
+	e2 := combination["e2"]
+	e3 := combination["e3"]
+	if e2 == "e" && e1 == "a" {
+		return false, nil
+	}
+	if e3 == "h" && e2 != "f" {
+		return false, nil
+	}
+	return true, nil
+}
+
+func Test_filterer_Filter(t *testing.T) {
+	cases := model.Cases{
+		"e1": {Options: map[string]model.Option{"a": {}, "b": {}}},
+		"e2": {Options: map[string]model.Option{"d": {}, "e": {If: "!e1.a"}, "f": {}}},
+		"e3": {Options: map[string]model.Option{"g": {}, "h": {If: "e2.f"}}},
+	}
+	parser := &mockParser{}
+	combinations := []model.Combination{
+		{"e1": "a", "e2": "d", "e3": "g"},
+		{"e1": "a", "e2": "d", "e3": "h"},
+		{"e1": "a", "e2": "e", "e3": "g"},
+		{"e1": "a", "e2": "e", "e3": "h"},
+		{"e1": "a", "e2": "f", "e3": "g"},
+		{"e1": "a", "e2": "f", "e3": "h"},
+		{"e1": "b", "e2": "d", "e3": "g"},
+		{"e1": "b", "e2": "d", "e3": "h"},
+		{"e1": "b", "e2": "e", "e3": "g"},
+		{"e1": "b", "e2": "e", "e3": "h"},
+		{"e1": "b", "e2": "f", "e3": "g"},
+		{"e1": "b", "e2": "f", "e3": "h"},
+	}
+
+	tests := []struct {
+		name    string
+		want    []model.Combination
+		wantErr bool
+	}{
+		{
+			name: "returns result as expected",
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "g"},
+				{"e1": "a", "e2": "f", "e3": "g"},
+				{"e1": "a", "e2": "f", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "g"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := filterer.New(cases, parser, combinations)
+			got, err := f.Filter()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("filterer.Filter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("filterer.Filter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
