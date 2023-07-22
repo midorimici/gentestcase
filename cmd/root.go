4ba0f7416b3daf715d9104daa015af626a9552cd
mici <60635066+midorimici@users.noreply.github.com>
2023-07-22 14:53:33 +0900

    ♻️ Refactor command codes

--- cmd/root.go
+++ cmd/root.go
@@ -6,17 +6,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "gentestcase",
-	Short: "CLI tool to generate combination test cases",
-}
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	// rootCmd represents the base command when called without any subcommands
+	var rootCmd = &cobra.Command{
+		Use:   "gentestcase",
+		Short: "CLI tool to generate combination test cases",
+	}
+	setSubcommands(rootCmd)
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
+	}
+}
+
+var subcommands = []func() *cobra.Command{
+	versionCmd,
+	runCmd,
+}
+
+func setSubcommands(rootCmd *cobra.Command) {
+	for _, c := range subcommands {
+		rootCmd.AddCommand(c())
 	}
 }
--- cmd/version.go
+++ cmd/version.go
@@ -9,14 +9,13 @@ import (
 const version = "1.3.2"
 
 // versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Show gentestcase version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version)
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(versionCmd)
+func versionCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Show gentestcase version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println(version)
+		},
+	}
+	return cmd
 }
