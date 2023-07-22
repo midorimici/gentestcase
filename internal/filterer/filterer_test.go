eb95df1136468125b5a9a8dc87aad5e512216af4
mici <60635066+midorimici@users.noreply.github.com>
2023-07-22 09:59:18 +0900

    ✅ Add tests of filterer

--- internal/filterer/filterer_test.go
+++ internal/filterer/filterer_test.go
@@ -27,16 +27,23 @@ func (p *mockParser) Parse(combination model.Combination, text string) (bool, er
 }
 
 func Test_filterer_Filter(t *testing.T) {
-	constraints := model.Constraints{
-		{
-			OnlyIf: "!e1.a",
-			Then:   "e2.e",
-		},
-		{
-			OnlyIf: "e2.f",
-			Then:   "e3.h",
-			Else:   "e3.g",
-		},
+	c1 := model.Constraint{
+		If:   "e1.a",
+		Then: "e2.d",
+	}
+	c2 := model.Constraint{
+		If:   "e2.e",
+		Then: "e3.g",
+		Else: "e3.h",
+	}
+	c3 := model.Constraint{
+		OnlyIf: "!e1.a",
+		Then:   "e2.e",
+	}
+	c4 := model.Constraint{
+		OnlyIf: "e2.f",
+		Then:   "e3.h",
+		Else:   "e3.g",
 	}
 	parser := &mockParser{}
 	combinations := []model.Combination{
@@ -55,12 +62,92 @@ func Test_filterer_Filter(t *testing.T) {
 	}
 
 	tests := []struct {
-		name    string
-		want    []model.Combination
-		wantErr bool
+		name        string
+		constraints model.Constraints
+		want        []model.Combination
+		wantErr     bool
 	}{
 		{
-			name: "returns result as expected",
+			name:        "returns result as expected with single condition",
+			constraints: model.Constraints{c1},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "g"},
+				{"e1": "a", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "g"},
+				{"e1": "b", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "e", "e3": "h"},
+				{"e1": "b", "e2": "f", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with single condition",
+			constraints: model.Constraints{c2},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "h"},
+				{"e1": "a", "e2": "e", "e3": "g"},
+				{"e1": "a", "e2": "f", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with single condition",
+			constraints: model.Constraints{c3},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "g"},
+				{"e1": "a", "e2": "d", "e3": "h"},
+				{"e1": "a", "e2": "f", "e3": "g"},
+				{"e1": "a", "e2": "f", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "g"},
+				{"e1": "b", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "e", "e3": "h"},
+				{"e1": "b", "e2": "f", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with single condition",
+			constraints: model.Constraints{c4},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "g"},
+				{"e1": "a", "e2": "e", "e3": "g"},
+				{"e1": "a", "e2": "f", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "g"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with multiple conditions",
+			constraints: model.Constraints{c1, c2},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with multiple conditions",
+			constraints: model.Constraints{c1, c3},
+			want: []model.Combination{
+				{"e1": "a", "e2": "d", "e3": "g"},
+				{"e1": "a", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "d", "e3": "g"},
+				{"e1": "b", "e2": "d", "e3": "h"},
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "e", "e3": "h"},
+				{"e1": "b", "e2": "f", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
+		{
+			name:        "returns result as expected with multiple conditions",
+			constraints: model.Constraints{c3, c4},
 			want: []model.Combination{
 				{"e1": "a", "e2": "d", "e3": "g"},
 				{"e1": "a", "e2": "f", "e3": "h"},
@@ -69,10 +156,18 @@ func Test_filterer_Filter(t *testing.T) {
 				{"e1": "b", "e2": "f", "e3": "h"},
 			},
 		},
+		{
+			name:        "returns result as expected with multiple conditions",
+			constraints: model.Constraints{c1, c2, c3, c4},
+			want: []model.Combination{
+				{"e1": "b", "e2": "e", "e3": "g"},
+				{"e1": "b", "e2": "f", "e3": "h"},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f := filterer.New(constraints, parser, combinations)
+			f := filterer.New(tt.constraints, parser, combinations)
 			got, err := f.Filter()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("filterer.Filter() error = %v, wantErr %v", err, tt.wantErr)
